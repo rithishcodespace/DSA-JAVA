5e1f35782dc99bcf774e32c66b1fb95349865a77
Rithish S <rithishcodespace@gmail.com>
2026-05-26 08:13:52 +0530

    Time: 1 ms (13.62%) | Memory: 41.8 MB (90.39%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,26 +1,30 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+        dp[m-1][n-1] = 1;
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+
+                // skip target cell
+                if(i == m-1 && j == n-1){
+                    continue;
+                }
+
+                int down = 0, right = 0;
+
+                if(i+1 < m){
+                    down = dp[i+1][j];
+                }
+
+                if(j+1 < n){
+                    right = dp[i][j+1];
+                }
+
+                dp[i][j] = down+right;
+            }
         }
-        return f(m,n,0,0,dp);
-    }
-    public int f(int m, int n, int i, int j, int[][] dp){
-        if(i == m-1 && j == n-1){
-            return 1; // 1 valid path found
-        }
-        if(i >= m || j >= n){
-            return 0; // out of bound
-        }
 
-        if(dp[i][j] != -1)return dp[i][j];
-
-        int down = f(m,n,i+1,j, dp);
-        int right = f(m,n,i,j+1, dp);
-
-        dp[i][j] = down+right;
-
-        return dp[i][j];
+        return dp[0][0];
     }
 }
