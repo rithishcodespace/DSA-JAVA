20a04c809816940cf307df15ab43d3f0e163f73e
Rithish S <rithishcodespace@gmail.com>
2026-05-28 08:54:00 +0530

    Time: 6 ms (10.47%) | Memory: 50.5 MB (21.64%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,28 +1,29 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         int[][] dp = new int[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return f(0,0,grid,dp);
-    }
-    public int f(int r, int c, int[][] grid, int[][] dp){
-        if(r == grid.length-1 && c == grid[0].length-1){
-            return grid[r][c];
-        }
-        if(r >= grid.length || c >= grid[0].length){
-            return (int)1e9;
+        // base case
+        dp[grid.length-1][grid[0].length-1] = grid[grid.length-1][grid[0].length-1];
+
+        for(int i=grid.length-1;i>=0;i--){
+            for(int j=grid[0].length-1;j>=0;j--){
+                if(i == grid.length-1 && j == grid[0].length-1){
+                    continue;
+                }
+
+                int down = (int)1e9, right = (int)1e9;
+
+                if(i+1 < grid.length){
+                    down = dp[i+1][j];
+                }
+
+                if(j+1 < grid[0].length){
+                    right = dp[i][j+1];
+                }
+
+                dp[i][j] = grid[i][j] + Math.min(down,right);
+            }
         }
 
-        if(dp[r][c] != -1){
-            return dp[r][c];
-        }
-
-        int down = grid[r][c] + f(r+1,c,grid,dp);
-        int right = grid[r][c] + f(r,c+1,grid,dp);
-
-        dp[r][c] = Math.min(down,right);
-
-        return dp[r][c];
+        return dp[0][0];
     }
 }
