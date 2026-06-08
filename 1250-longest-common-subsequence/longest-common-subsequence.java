d29fe91c89a2e4dbf00f31bca369a4709622eec7
Rithish S <rithishcodespace@gmail.com>
2026-06-08 19:51:55 +0530

    Time: 20 ms (78.44%) | Memory: 54 MB (34.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,30 +1,25 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        Integer[][] dp = new Integer[text1.length()][text2.length()];
-        return solve(text1.length()-1, text2.length()-1, text1, text2, dp);
-    }
-    public int solve(int idx1, int idx2, String text1, String text2, Integer[][] dp){
-        if(idx1 < 0 || idx2 < 0){
-            return 0;
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = Math.max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    );
+                }
+            }
         }
 
-        if(dp[idx1][idx2] != null){
-            return dp[idx1][idx2];
-        }
-
-        int match = 0, not_match1 = 0, not_match2 = 0;
-
-        // match
-        if(text1.charAt(idx1) == text2.charAt(idx2)){
-            match = 1 + solve(idx1-1, idx2-1, text1, text2, dp);
-        }
-        else{ // not match
-            not_match1 = 0 + solve(idx1-1, idx2, text1, text2, dp);
-            not_match2 = solve(idx1, idx2-1, text1, text2, dp);
-        }
-
-        dp[idx1][idx2] = (match + Math.max(not_match1, not_match2));
-
-        return dp[idx1][idx2];
+        return dp[n][m];
     }
 }
