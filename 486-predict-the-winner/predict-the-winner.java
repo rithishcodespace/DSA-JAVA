7ce9f7f7df9eb5f6ae5c9a3432dd5ee5e400978e
Rithish S <rithishcodespace@gmail.com>
2026-06-14 18:49:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (56.46%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,16 +1,21 @@
 class Solution {
-    public boolean predictTheWinner(int[] nums) {
-        return solve(0, nums.length - 1, nums) >= 0;
+    public boolean predictTheWinner(int[] nums){
+        Integer[][] dp = new Integer[nums.length][nums.length];
+        return solve(0, nums.length - 1, nums, dp) >= 0;
     }
     // 1'st player - 2'nd players score
-    public int solve(int i, int j, int[] nums) { 
+    public int solve(int i, int j, int[] nums, Integer[][] dp){ 
         if(i == j){ // i have one option to pick (deciding factor)
             return nums[i];
         }
 
-        int pickLeft = nums[i] - solve(i+1, j, nums);
-        int pickRight = nums[j] - solve(i, j-1, nums);
+        if(dp[i][j] != null)return dp[i][j];
 
-        return Math.max(pickLeft, pickRight);
+        int pickLeft = nums[i] - solve(i+1, j, nums, dp); 
+        int pickRight = nums[j] - solve(i, j-1, nums, dp);
+
+        dp[i][j] = Math.max(pickLeft, pickRight);
+
+        return dp[i][j];
     }
 }
