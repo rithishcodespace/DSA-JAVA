f8f6851d6a64d75ea4b0ba68b1719844f819dc4f
Rithish S <rithishcodespace@gmail.com>
2026-07-06 10:08:10 +0530

    Time: 0 ms (100%), Space: 45.2 MB (12.53%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,62 @@
+// after finding mid, find which half (l to mid) or (mid+1 to r) is sorted
+// if sorted, check whether target might exists on that range
+// if exists, continue binary search inside that range
+
+// tc: O(logn)
+// sc: O(1)
+
+// copy pasted version I's code
+// edge case we face here is when nums[l] == nums[mid] == nums[r], to solve we can shrink our search space to l=l+1, r=r-1
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l=0, r=nums.length-1;
+
+        while(l <= r){
+            int mid = l+(r-l)/2;
+
+            if(nums[mid] == target)return true;
+            if(nums[l] == nums[mid] && nums[mid] == nums[r]){
+                l=l+1;
+                r=r-1;
+                continue;
+            }
+
+            // check which portion is sorted
+            if(nums[l]<=nums[mid]){
+                if(nums[l] <= target && nums[mid] >= target){
+                    if(nums[mid] == target){
+                        return true;
+                    }
+                    else if(nums[mid] > target){
+                        r=mid-1;
+                    }
+                    else{
+                        l=mid+1;
+                    }
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && nums[r] >= target){
+                    if(nums[mid] == target){
+                        return true;
+                    }
+                    else if(nums[mid] > target){
+                        r=mid-1;
+                    }
+                    else{
+                        l=mid+1;
+                    }
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+        }   
+
+        return false;
+    }
+}
