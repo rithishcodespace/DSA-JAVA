06bbf6de88de135a17e0119c028db0c676551f51
Rithish S <rithishcodespace@gmail.com>
2026-07-08 14:09:40 +0530

    Time: 0 ms (100%), Space: 46.6 MB (72.24%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,23 @@
+// tortoise and hare
+
+// both will me at a point, since distance first increase, after meeting a cycle their distance decreases and eventually reaches 0
+
+// tc: O(n)
+// sc: O(1)
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null)return false;
+
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)return true;
+        }   
+
+        return false;     
+    }
+}
