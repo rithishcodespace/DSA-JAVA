f1acfa6a0df018adb151a0f352aff6832447a65f
Rithish S <rithishcodespace@gmail.com>
2026-07-10 14:48:52 +0530

    Time: 88 ms (89.35%), Space: 71.5 MB (10.19%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,34 +1,38 @@
-// sieve of erathroses
+// when the factors exceeds root(n), we will again get the same relation, it is better to stop here
+
+// eg: 36
+// 36 = 1 × 36
+// 36 = 2 × 18
+// 36 = 3 × 12
+// 36 = 4 × 9
+// 36 = 6 × 6
+// 36 = 9 × 4
+// 36 = 12 × 3
+// 36 = 18 × 2
+// 36 = 36 × 1
 
 class Solution {
     public int countPrimes(int n) {
         if(n <= 1)return 0;
-        boolean[] arr = new boolean[n];
 
-        for(int i=2;i*i <= n;i++){
-            if(isPrime(i)){
-                // mark multiplies of i
-                for(int j=2;j*i < n;j++){
-                    arr[j*i] = true;
-                }                                               
+        int[] primes = new int[n];
+        Arrays.fill(primes,1);
+        primes[0] = 0;
+        primes[1] = 0;
+
+        for(int i=2;i<Math.sqrt(n);i++){
+            if(primes[i] != 0){ // multiples of primes are always composite
+                for(int j=i*i;j<n;j+=i){
+                    primes[j] = 0;
+                }
             }
         }
 
-        // count non false cells
-        int cnt = 0;
-        for(int i=2;i<arr.length;i++){
-            if(!arr[i])cnt++;
+        int count = 0;
+        for(int i=0;i<primes.length;i++){
+            count += primes[i];
         }
 
-        return cnt;
-    }
-    public boolean isPrime(int n){
-        if(n == 2)return true;
-
-        for(int i=2;i<n;i++){
-            if(n%i == 0)return false;
-        }
-
-        return true;
+        return count;
     }
 }
