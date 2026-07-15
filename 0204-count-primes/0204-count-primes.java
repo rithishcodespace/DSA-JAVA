ead1e84ce1dfdc5a2ad3a0ce2cf155688fe34682
Rithish S <rithishcodespace@gmail.com>
2026-07-15 14:08:29 +0530

    Time: 103 ms (58.48%), Space: 51.3 MB (23.62%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,36 +1,23 @@
-// when the factors exceeds root(n), we will again get the same relation, it is better to stop here
-
-// eg: 36
-// 36 = 1 × 36
-// 36 = 2 × 18
-// 36 = 3 × 12
-// 36 = 4 × 9
-// 36 = 6 × 6
-// 36 = 9 × 4
-// 36 = 12 × 3
-// 36 = 18 × 2
-// 36 = 36 × 1
-
 class Solution {
     public int countPrimes(int n) {
         if(n <= 1)return 0;
 
-        int[] primes = new int[n];
-        Arrays.fill(primes,1);
-        primes[0] = 0;
-        primes[1] = 0;
+        boolean[] primes = new boolean[n]; 
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
 
         for(int i=2;i<Math.sqrt(n);i++){
-            if(primes[i] != 0){ // multiples of primes are always composite
+            if(primes[i] == true){
                 for(int j=i*i;j<n;j+=i){
-                    primes[j] = 0;
+                    primes[j] = false;
                 }
             }
         }
 
-        int count = 0;
-        for(int i=0;i<primes.length;i++){
-            count += primes[i];
+        int count=0;
+        for(int i=0;i<n;i++){
+            count += primes[i] ? 1 : 0;
         }
 
         return count;
