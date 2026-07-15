419c63528a47b19faf787bb1b45c56155ece05b8
Rithish S <rithishcodespace@gmail.com>
2026-07-15 11:27:06 +0530

    Time: 1 ms (83.11%), Space: 42.5 MB (74.78%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,10 @@
+// sum of odd numbers -> n^2
+// sum of even numbers -> n(n+1)
+
+// since n is common in both, n is the gcd
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
