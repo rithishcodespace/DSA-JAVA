11f4f1ea92841f1d18c482c8086f2cab9170b3d1
Rithish S <rithishcodespace@gmail.com>
2026-07-17 07:44:14 +0530

    Time: 9 ms (18.16%), Space: 43.2 MB (42.66%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,26 +1,20 @@
-// pick every even
-// if odd (pick even length)
-// pick exactly one char in left over odds -> (odd-1)
-
-// tc: O(n)
-// sc: O(n)
-
 class Solution {
     public int longestPalindrome(String s) {
-        int even = 0, odd = 0;
+        int len = 0, odd = 0;
         HashMap<Character,Integer> map = new HashMap<>();
+
         for(int i=0;i<s.length();i++){
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
-        }
+        }  
 
         for(char key : map.keySet()){
-            if(map.get(key)%2 == 0)even += map.get(key);
+            if(map.get(key)%2 == 0)len += map.get(key);
             else{
-                odd = 1;
-                even += map.get(key)-1;
+                len += map.get(key)-1;
+                odd=1;
             }
-        }
+        }     
 
-        return even+odd;
+        return len+odd;
     }
 }
