9e3226cdcce8b5aca7c8141033ec44528ba65575
Rithish S <rithishcodespace@gmail.com>
2026-07-18 13:36:20 +0530

    Time: 236 ms (9.81%), Space: 46.9 MB (7.59%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,13 +1,14 @@
-// DFS
+// Time Complexity  : O(m × n × 3^L)
+// Space Complexity : O(m × n + L)
 
 class Solution {
+    int[][] visited;
     public boolean exist(char[][] board, String word) {
+        visited = new int[board.length][board[0].length];
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j] == word.charAt(0)){
-                    int[][] visited = new int[board.length][board[0].length];
-                    visited[i][j] = 1;
-                    if(dfs(i, j, 1, word, board, visited)){
+                    if(dfs(i, j, 1, word, board)){
                         return true;
                     }
                 }
@@ -15,23 +16,24 @@ class Solution {
         }
         return false;
     }
-    public boolean dfs(int row, int col, int idx, String word, char[][] board, int[][] visited){
-
+    public boolean dfs(int r, int c, int idx, String word, char[][] board){
         if(idx == word.length())return true;
 
-        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
-        for(int[] dir : directions){
-            int nr = row + dir[0];
-            int nc = col + dir[1];
+        for(int[] d : dir){
+            int nr = r + d[0];
+            int nc = c + d[1];
 
-            if(nr >= 0 && nr < board.length && nc >= 0 && nc < board[0].length && visited[nr][nc] == 0 && board[nr][nc] == word.charAt(idx)){
-                visited[nr][nc] = 1;
-                if(dfs(nr, nc, idx+1, word, board, visited)){
-                    return true;
+            if(nr >= 0 && nr < board.length && nc >= 0 && nc < board[0].length){
+                if(board[nr][nc] == word.charAt(idx) && visited[nr][nc] == 0){
+                    visited[r][c] = 1;
+                    if(dfs(nr, nc, idx+1, word, board)){
+                        visited[r][c] = 0; // backtrack
+                        return true;
+                    }
+                    visited[r][c] = 0; // backtrack
                 }
-                // backtrack
-                visited[nr][nc] = 0;
             }
         }
 
