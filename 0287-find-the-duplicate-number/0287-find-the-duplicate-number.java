573c26f90a2110dd0708bbcda2115988cc226a8e
Rithish S <rithishcodespace@gmail.com>
2026-07-19 22:04:09 +0530

    Time: 4 ms (90.74%), Space: 82.7 MB (77.65%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,56 +1,22 @@
-// Sorting is not allowed (modifies the array).
-// HashSet uses O(n) extra space.
-
-// cycle detection with tortoise and hare
-// Instead of storing pointers like a linked list, let the value point to the next index.
-
-// 0 → 1
-// 1 → 3
-// 3 → 2
-// 2 → 4
-// 4 → 2
-
-// 0 → 1 → 3 → 2 → 4
-//            ↑_____|
-
-// linkedlist cycle 2
-// find the position where both pointers collide
-// find the starting point of the cycle
-//  disntance between start node to cycle point and cycle point to collision point is same
-
-// tc: O(n)
-// sc: O(1)
-
 class Solution {
     public int findDuplicate(int[] nums) {
         int slow = 0;
         int fast = 0;
 
-        // find the collision point
+        // find collision point
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
         }
         while(slow != fast);
 
-        // find the starting point
+        // find starting point of cycle
         slow = 0;
-        do{
+        while(slow != fast){
             slow = nums[slow];
             fast = nums[fast];
         }
-        while(slow != fast);
 
         return slow;
     }
 }
-
-
-
-
-
-
-
-
-
-
