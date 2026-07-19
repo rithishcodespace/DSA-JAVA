cc02b2154cb15bd136b1aca804fcdd025732e3ae
Rithish S <rithishcodespace@gmail.com>
2026-07-19 21:45:39 +0530

    Time: 0 ms (100%), Space: 43.7 MB (65.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,10 +1,3 @@
-// after finding mid, find which half (l to mid) or (mid+1 to r) is sorted
-// if sorted, check whether target might exists on that range
-// if exists, continue binary search inside that range
-
-// tc: O(logn)
-// sc: O(1)
-
 class Solution {
     public int search(int[] nums, int target) {
         int l=0, r=nums.length-1;
@@ -14,40 +7,19 @@ class Solution {
 
             if(nums[mid] == target)return mid;
 
-            // check which portion is sorted
-            if(nums[l]<=nums[mid]){
-                if(nums[l] <= target && nums[mid] >= target){
-                    if(nums[mid] == target){
-                        return mid;
-                    }
-                    else if(nums[mid] > target){
-                        r=mid-1;
-                    }
-                    else{
-                        l=mid+1;
-                    }
+            if(nums[l] <= nums[mid]){
+                if(target >= nums[l] && target <= nums[mid]){
+                    r=mid;
                 }
-                else{
-                    l=mid+1;
-                }
+                else l=mid+1;
             }
-            else if(nums[mid] < nums[r]){
-                if(nums[mid] <= target && nums[r] >= target){
-                    if(nums[mid] == target){
-                        return mid;
-                    }
-                    else if(nums[mid] > target){
-                        r=mid-1;
-                    }
-                    else{
-                        l=mid+1;
-                    }
+            else{
+                if(target >= nums[mid] && target <= nums[r]){
+                    l=mid;
                 }
-                else{
-                    r=mid-1;
-                }
+                else r=mid-1;
             }
-        }   
+        }
 
         return -1;
     }
