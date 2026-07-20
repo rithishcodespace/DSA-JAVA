aabcb6223f7278c506325b93651f9264215f80b2
Rithish S <rithishcodespace@gmail.com>
2026-07-20 14:14:35 +0530

    Time: 0 ms (100%), Space: 46.9 MB (7.21%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -1,27 +1,17 @@
-// each node contains the sum of their Max child + them
-// if sum reaches neg, update max, start from 0 again
-
-// a path is like a straight road, that cannot branch
-
-// tc: O(n)
-// sc: O(tree height)
-
 class Solution {
     int max = Integer.MIN_VALUE;
-
     public int maxPathSum(TreeNode root) {
-        postorder(root);
+        preorder(root);
         return max;
     }
-
-    public int postorder(TreeNode root){
+    public int preorder(TreeNode root){
         if(root == null)return 0;
 
-        int left = postorder(root.left);
-        int right = postorder(root.right);
+        int left = Math.max(0,preorder(root.left));
+        int right = Math.max(0,preorder(root.right));
 
-        max = Math.max(max, root.val + left + right); // a single root, can pick both left and right
+        max = Math.max(max, left + root.val + right);
 
-        return root.val+Math.max(left, right) > 0 ? root.val+Math.max(left, right) : 0;
+        return root.val + Math.max(left, right);
     }
 }
