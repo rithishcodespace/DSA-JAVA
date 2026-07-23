85b94183502e49682447fa2f1f27a15a170153a6
Rithish S <rithishcodespace@gmail.com>
2026-07-23 18:43:35 +0530

    Time: 38 ms (66.5%), Space: 99 MB (19.75%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -1,21 +1,16 @@
-// we go from n-1 to 0, coz we would be standing on nums[i], so it would to find whether nums[j] and nums[k]
-// stack stores nums[j] (greatest in the pattern), and eventually every visited number could become nums[j]
-// third stores nums[k]
-
 class Solution {
     public boolean find132pattern(int[] nums) {
-        int n = nums.length;
-        int third = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE, n = nums.length;
         Stack<Integer> stack = new Stack<>();
 
         for(int i=n-1;i>=0;i--){
-            if(nums[i] < third)return true; // nums[i] < nums[k] < nums[j]
+            if(nums[i] < third)return true;
 
-            while(!stack.isEmpty() && nums[i] > stack.peek()){ // monotonically increasing array
-                third = stack.pop(); // better candidate for nums[k]
+            while(!stack.isEmpty() && nums[i] > stack.peek()){
+                third = stack.pop();
             }
 
-            stack.push(nums[i]); // not it became nums[j]
+            stack.push(nums[i]);
         }
 
         return false;
