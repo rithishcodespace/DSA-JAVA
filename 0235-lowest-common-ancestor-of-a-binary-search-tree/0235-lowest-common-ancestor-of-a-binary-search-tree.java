8603dc9a29cb9cb535db055141e76ff8ac4c5f9b
Rithish S <rithishcodespace@gmail.com>
2026-07-23 19:57:21 +0530

    Time: 6 ms (97.11%), Space: 47.5 MB (75.21%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,28 +1,20 @@
-// tc: O(n)
-// sc: O(tree height)
-
-// do my children contain p and q ?
-
 class Solution {
-
     TreeNode lca = null;
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         postorder(root, p, q);
-        return lca;       
+        return lca;
     }
-
     public boolean postorder(TreeNode root, TreeNode p, TreeNode q){
         if(root == null)return false;
 
         boolean left = postorder(root.left, p, q);
         boolean right = postorder(root.right, p, q);
 
-        if(left && right){
-            if(lca == null)lca = root; // case 1
+        if(left && right){ // case 1 - 3 nodes
+            if(lca == null)lca = root;
         }
-        else if((left || right) && (root.val == p.val || root.val == q.val)){
-            lca = root; // case 2
+        else if((left || right) && (root.val == p.val || root.val == q.val)){ // case 2 - only 2 nodes
+            if(lca == null)lca = root;
         }
 
         return (root.val == p.val || root.val == q.val) || (left || right);
