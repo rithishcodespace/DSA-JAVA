fc6f4b4a0f95898ccd2de25907a005ad4b51c8aa
Rithish S <rithishcodespace@gmail.com>
2026-07-24 18:49:13 +0530

    Time: 4 ms (74.27%), Space: 45.2 MB (62.02%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,29 +1,22 @@
-// instead of storing next greater in array, used hmap to cut off the nested loop used in previous submission
-
-// tc: O(nums1 + nums2)
-// sc: O(n)
-
-// monotonically decreasing stack (back to front)
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        Stack<Integer> stack = new Stack<>();
         HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
 
         for(int i=nums2.length-1;i>=0;i--){
-            while(!stack.isEmpty() && stack.peek() < nums2[i]){
+            while(!stack.isEmpty() && nums2[i] >= stack.peek()){
                 stack.pop();
             }
-            map.put(nums2[i], (stack.isEmpty()) ? -1 : stack.peek());
+
+            map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
             stack.push(nums2[i]);
         }
 
-        // reduced to single loop via map
-        for(int i=0;i<nums1.length;i++){
-            ans[i] = map.get(nums1[i]);
+        int[] arr = new int[nums1.length];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = map.get(nums1[i]);
         }
 
-        return ans;
+        return arr;
     }
 }
