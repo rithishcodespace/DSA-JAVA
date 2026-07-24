20d88408381974c72950eb8e00204eb08eed17ed
Rithish S <rithishcodespace@gmail.com>
2026-07-24 18:43:45 +0530

    Time: 2 ms (92.1%), Space: 47.8 MB (57.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,12 @@
-// two pointers
-// swap every non zero element to the j'th pointer
-// i -> scan's entire array to find non zero index
-// j -> says element where non-zero elemnt to be placed
-
-// tc: O(n)
-// sc: O(1)
-
 class Solution {
+    int j=0;
     public void moveZeroes(int[] nums) {
-        int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i] != 0){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                j++; // next index to place a non-zero element
+                j++;
             }
         }
     }
