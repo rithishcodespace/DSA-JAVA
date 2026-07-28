71812d43e7bdbb146e3e912dd2fe909c57c16f14
Rithish S <rithishcodespace@gmail.com>
2026-07-28 09:11:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,35 +1,28 @@
-// fix one element, then solve Two Sum II on the rest + instead of zero, find closest to target
-
-// tc: O(logn) + O(n^2) -> O(n^2)
-// sc : O(1)   (excluding the output list)
-
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int ans = nums[0] + nums[1] + nums[2];;
-
-        // convert to TWO SUM II
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++){ // each range should have >= 3 numbers
-            int x = i, y = i+1, z = nums.length-1;
+        int ans = nums[0]+nums[1]+nums[2];
 
-            while(y<z){
-                int sum = nums[x]+nums[y]+nums[z];
+        for(int i=0;i<nums.length;i++){
+            int l=i+1, r=nums.length-1;
 
-                // maintain answer
-                int curr_diff = Math.abs(sum-target);
-                int ans_diff = Math.abs(ans-target);
-                if(curr_diff < ans_diff){
-                    ans = sum;
-                }
-
+            while(l<r){
+                int sum = nums[i]+nums[l]+nums[r];
+                int diff = target-sum, ans_diff = target-ans;
+                
                 if(sum == target){
-                    return target;
+                    return sum;
                 }
-                else if(sum > target){
-                    z--; // r -> reduces sum
+                else if(sum < target){
+                    l++;
                 }
                 else{
-                    y++; // l -> increases sum
+                    r--;
+                }
+
+                if(ans_diff > diff){
+                    ans = sum;
+                    ans_diff = diff;
                 }
             }
         }
