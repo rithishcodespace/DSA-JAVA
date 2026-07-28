20e25f5b1f2246c81cbead572c915a4ec14e06c0
Rithish S <rithishcodespace@gmail.com>
2026-07-28 16:18:26 +0530

    Time: 25 ms (78.24%), Space: 44.2 MB (59.89%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -1,51 +1,26 @@
-// traverse original graph, don't push new nodes in the queue
-
 class Solution {
     public Node cloneGraph(Node node) {
         if(node == null)return null;
 
-        HashMap<Node,Node> map = new HashMap<>();
-        map.put(node, new Node(node.val, new ArrayList<>())); // origina -> copy
-
+        HashMap<Node, Node> map = new HashMap<>();
         Queue<Node> queue = new LinkedList<>();
+
+        map.put(node, new Node(node.val, new ArrayList<>()));
         queue.add(node);
 
         while(!queue.isEmpty()){
             Node curr = queue.poll();
 
             for(Node neigh : curr.neighbors){
-               // if not cloned, clone it - acts as visited node
-               if(!map.containsKey(neigh)){
-                 map.put(neigh, new Node(neigh.val, new ArrayList<>()));
-                 queue.add(neigh);
-               }
+                if(!map.containsKey(neigh)){
+                    map.put(neigh, new Node(neigh.val, new ArrayList<>()));
+                    queue.add(neigh);
+                }
 
-               // connect neigh of original to clone
-               map.get(curr).neighbors.add(map.get(neigh));
+                map.get(curr).neighbors.add(map.get(neigh));
             }
-
         }
 
         return map.get(node);
     }
 }
-
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
