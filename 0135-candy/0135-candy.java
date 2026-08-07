60a05d4cab5e3dd0d78b421c5eebb8de3db00c30
Rithish S <rithishcodespace@gmail.com>
2026-08-07 18:18:55 +0530

    Time: 4 ms (20.16%), Space: 47.6 MB (51.72%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,29 +1,30 @@
-// tc: O(2n)
-// sc: O(n)
-
 class Solution {
     public int candy(int[] ratings) {
-      int candies = 0, prevR = 1;
+        int candies = 0;
+        int[] left = new int[ratings.length];
+        int[] right = new int[ratings.length];
 
-      int[] left = new int[ratings.length];
-
-      // if i dont have left element or i'm lesser than left - give 1  
-      for(int i=0;i<ratings.length;i++){
-        if(i == 0 || ratings[i-1] >= ratings[i])left[i] = 1;
-        else left[i] = left[i-1]+1;
-      }
-      // if i dont have right element or i'm lesser than right - give 1   
-      for(int i=ratings.length-1;i>=0;i--){
-        if(i == ratings.length-1 || ratings[i+1] >= ratings[i]){
-            candies += Math.max(left[i],1);
-            prevR = 1;
+        // left neigh check
+        for(int i=0;i<ratings.length;i++){
+            if(i > 0 && ratings[i-1] < ratings[i]){
+                left[i] = left[i-1]+1;
+            }
+            else left[i] = 1;
         }
-        else{
-            candies += Math.max(left[i], prevR+1);
-            prevR = prevR+1;
-        }
-      }
 
-      return candies;
+        // right neigh check
+        for(int i=ratings.length-1;i>=0;i--){
+            if(i<ratings.length-1 && ratings[i] > ratings[i+1]){
+                right[i] = right[i+1]+1;
+            }
+            else right[i] = 1;
+        }
+
+        // take max
+        for(int i=0;i<ratings.length;i++){
+            candies += Math.max(left[i], right[i]);
+        }
+
+        return candies;
     }
 }
