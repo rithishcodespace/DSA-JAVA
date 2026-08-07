ed243d4de93fc21be6c89a5da1af7ea987c9dd52
Rithish S <rithishcodespace@gmail.com>
2026-08-07 18:40:16 +0530

    Time: 2 ms (89.93%), Space: 44.3 MB (24.99%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,45 +1,46 @@
-// multi-source bfs
-
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int min = -1, fresh = 0;
-        int[][] dir = {{-1,0}, {1,0}, {0,1}, {0,-1}};
-        Queue<Cell> queue = new LinkedList<>();
+        int minute = -1, fresh = 0;
 
-        // insert the rotten oranges at 0'th min
+        Queue<Cell> queue = new LinkedList<>();
+        Integer[][] visited = new Integer[grid.length][grid[0].length];
+
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == 2){
                     queue.add(new Cell(i, j));
+                    visited[i][j] = 1;
                 }
                 else if(grid[i][j] == 1)fresh++;
             }
         }
-
+        
         if(fresh == 0)return 0;
+
+        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
 
         while(!queue.isEmpty()){
             int n = queue.size();
+
             for(int i=0;i<n;i++){
-                Cell cell = queue.poll();
+                Cell c = queue.poll();
 
                 for(int[] d : dir){
-                    int nr = cell.r + d[0];
-                    int nc = cell.c + d[1];
+                    int nr = d[0] + c.r;
+                    int nc = d[1] + c.c;
 
-                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length){
-                        if(grid[nr][nc] == 1){
-                            grid[nr][nc] = 2;
-                            fresh--;
-                            queue.add(new Cell(nr, nc));
-                        }
+                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && visited[nr][nc] == null && grid[nr][nc] == 1){
+                        queue.add(new Cell(nr, nc));
+                        visited[nr][nc] = 1;
+                        fresh--;
                     }
                 }
             }
-            min++;
+
+            minute++;
         }
 
-        return (fresh == 0) ? min : -1;
+        return fresh == 0 ? minute : -1;
     }
 }
 
