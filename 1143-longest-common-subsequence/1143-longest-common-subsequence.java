f7cd001d183f1ae2754e3cba49f5afa6240670ab
Rithish S <rithishcodespace@gmail.com>
2026-08-24 09:33:31 +0530

    Time: 20 ms (78.32%), Space: 53 MB (90.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,18 +1,19 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        Integer[][] dp = new Integer[text1.length()][text2.length()];
-        return solve(0, text1, 0, text2, dp);   
-    }
-    public int solve(int idx1, String text1, int idx2, String text2, Integer[][] dp){
-        if(idx1 >= text1.length() || idx2 >= text2.length()){
-            return 0;
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
         }
 
-        if(dp[idx1][idx2] != null)return dp[idx1][idx2];
-
-        char ch1 = text1.charAt(idx1);
-        char ch2 = text2.charAt(idx2);
-
-        return dp[idx1][idx2] = (ch1 == ch2) ? 1+solve(idx1+1, text1, idx2+1, text2, dp) :  Math.max(solve(idx1, text1, idx2+1, text2, dp), solve(idx1+1, text1, idx2, text2, dp));
+        return dp[m][n];
     }
 }
