342b20bc4ae439b749fe6d1a20ed2d74743fa227
Rithish S <rithishcodespace@gmail.com>
2026-08-26 22:30:45 +0530

    Time: 315 ms (49.5%), Space: 98.6 MB (23.81%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,36 +1,70 @@
-// minimum spanning tree - prims algo
+// minimum spanning tree - kruskal algo
 
 // v -> points[v][0], points[v][0]
 
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int mst = 0;
+        int mst = 0, edgesUsed = 0;
 
-        boolean[] visited = new boolean[points.length];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]); // [vertex, cost]
+        int[] size = new int[points.length];
+        int[] parent = new int[points.length];
 
-        // insert any vertex
-        pq.add(new int[]{0, 0});
+        for(int i=0;i<points.length;i++){
+            size[i] = 1;
+            parent[i] = i;
+        }
 
-        while(!pq.isEmpty()){
-            int[] cell = pq.poll();
-            int u = cell[0];
-            int d = cell[1];
+        List<int[]> edges = new ArrayList<>();
 
-            if(!visited[u]){
-                visited[u] = true; // i got process, so mark as visited
-                mst += d;
-
-                // insert my connections (since there is no edge, all vertex's are connected to me)
-                for(int v=0;v<points.length;v++){
-                    if(!visited[v]){
-                        int distance = Math.abs(points[u][0] - points[v][0]) + Math.abs(points[u][1] - points[v][1]);
-                        pq.add(new int[]{v, distance});
-                    }
-                }
+        for(int i=0;i<points.length;i++){
+            for(int j=i+1;j<points.length;j++){
+                int distance = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                edges.add(new int[]{i, j, distance});
             }
         }
 
+        Collections.sort(edges, (a,b) -> a[2] - b[2]);
+
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int d = edge[2];
+
+            int pu = find(u, parent);
+            int pv = find(v, parent);
+
+            if(pu != pv){
+                mst += d;
+                edgesUsed++;
+
+                union(pu, pv, parent, size);
+            }
+
+            if(edgesUsed == points.length-1)break;
+        }
+
         return mst;
+    }
+    public int find(int node, int[] parent){
+        if(parent[node] != node){
+            parent[node] = find(parent[node], parent); // path compression
+        }
+
+        return parent[node];
+    }
+    public void union(int u, int v, int[] parent, int[] size){
+        int pu = find(u, parent);
+        int pv = find(v, parent);
+
+        if(pu == pv)return;
+
+        if(size[pu] > size[pv]){
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+        else{
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
     }
 }
