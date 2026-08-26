0308c1999422a9a93a80dd22f3c67c899d8e406e
Rithish S <rithishcodespace@gmail.com>
2026-08-26 09:49:09 +0530

    Time: 4 ms (91.37%), Space: 46.5 MB (97.54%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,46 +1,56 @@
-// recursive topo sort
-// 0 → not visited
-// 1 → currently visiting
-// 2 → completely finished
+// we are just checking is it a DAG via toposort
+
+// 0 -> not visited
+// 1 -> currently visiting
+// 2 -> visited
+
+// If we encounter a cycle, we stop
 
 class Solution {
-    
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] visited = new int[numCourses];
         // create adj list
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<numCourses;i++)adj.add(new ArrayList<>());
 
-        for(int[] edge : prerequisites){
-            int u = edge[1];
-            int v = edge[0];
+        List<List<Integer>> adj_list = new ArrayList<>();
 
-            adj.get(u).add(v);
+        for(int i=0;i<numCourses;i++){
+            adj_list.add(new ArrayList<>());
         }
 
-        // dfs
-        for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 0) {
-                if(dfs(i, visited, adj)){
-                    return false;
-                }
+        for(int i=0;i<prerequisites.length;i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            adj_list.get(u).add(v);
+        }
+
+        // recursive topo sort
+
+        int[] visited = new int[numCourses];
+        
+        for(int i=0;i<numCourses;i++){
+            if(!dfs(i, adj_list, visited)){
+                return false;
             }
         }
 
         return true;
     }
-    public boolean dfs(int u, int[] visited, List<List<Integer>> adj){
+    public boolean dfs(int u, List<List<Integer>> adj_list, int[] visited){
         visited[u] = 1;
 
-        for(int v : adj.get(u)){
+        for(int v : adj_list.get(u)){
             if(visited[v] == 0){
-                if(dfs(v, visited, adj))return true;
+                if(!dfs(v, adj_list, visited)){
+                    return false;
+                }
             }
-            else if(visited[v] == 1)return true;
+            else if(visited[v] == 1){
+                return false;
+            }
         }
-
+        
         visited[u] = 2;
 
-        return false;
+        return true;
     }
 }
