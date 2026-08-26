6800c48d6236a05373db6ab67781acd143e69cb3
Rithish S <rithishcodespace@gmail.com>
2026-08-26 20:23:52 +0530

    Time: 48 ms (33.59%), Space: 189.1 MB (7.36%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -0,0 +1,58 @@
+// multi-source bfs - exactly same as 01 matrix
+
+class Solution {
+    int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
+
+    public int[][] highestPeak(int[][] mat) {
+        int[][] distance = new int[mat.length][mat[0].length]; // visited + ans
+        int[][] visited = new int[mat.length][mat[0].length];
+
+        // insert all destination cells (0)
+        Queue<Cell> queue = new LinkedList<>();
+
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j] == 1){
+                    queue.add(new Cell(i, j, 0));
+                    visited[i][j] = 1;
+                }
+            }
+        }
+
+        // multi_source_bfs
+
+        while(!queue.isEmpty()){
+            Cell cell = queue.poll();
+
+            int r = cell.r;
+            int c = cell.c;
+            int dist = cell.d;
+
+            distance[r][c] = dist;
+
+            for(int[] d : dir){
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr >= 0 && nr < mat.length && nc >= 0 && nc < mat[0].length){
+                    if(visited[nr][nc] == 0){
+                        queue.add(new Cell(nr, nc, dist+1));
+                        visited[nr][nc] = 1;
+                    }
+                }
+            }
+        }
+
+        return distance;
+    }
+}
+
+class Cell{
+    int r, c, d;
+
+    Cell(int r, int c, int d){
+        this.r = r;
+        this.c = c;
+        this.d = d;
+    }
+}
