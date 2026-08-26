efbc7dc7af77818d65380a1d32c44fc86b86435e
Rithish S <rithishcodespace@gmail.com>
2026-08-26 07:37:36 +0530

    Time: 2 ms (89.58%), Space: 44.2 MB (56.27%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,35 +1,38 @@
+// mutli-source bfs
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int minute = -1, fresh = 0;
 
-        Queue<Cell> queue = new LinkedList<>();
-        Integer[][] visited = new Integer[grid.length][grid[0].length];
+        int[][] dir = {{-1,0}, {1,0}, {0, -1}, {0,1}};
 
+        int[][] visited = new int[grid.length][grid[0].length];
+        Queue<Cell> queue = new LinkedList<>();
+
+        // insert all rotten oranges
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == 1)fresh++;
                 if(grid[i][j] == 2){
                     queue.add(new Cell(i, j));
-                    visited[i][j] = 1;
                 }
-                else if(grid[i][j] == 1)fresh++;
             }
         }
-        
+
         if(fresh == 0)return 0;
 
-        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
-
+        // BFS
         while(!queue.isEmpty()){
-            int n = queue.size();
+            int size = queue.size();
 
-            for(int i=0;i<n;i++){
+            for(int i=0;i<size;i++){
                 Cell c = queue.poll();
 
                 for(int[] d : dir){
-                    int nr = d[0] + c.r;
-                    int nc = d[1] + c.c;
+                    int nr = c.r + d[0];
+                    int nc = c.c + d[1];
 
-                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && visited[nr][nc] == null && grid[nr][nc] == 1){
+                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && visited[nr][nc] == 0 && grid[nr][nc] == 1){
                         queue.add(new Cell(nr, nc));
                         visited[nr][nc] = 1;
                         fresh--;
@@ -43,10 +46,8 @@ class Solution {
         return fresh == 0 ? minute : -1;
     }
 }
-
 class Cell{
     int r, c;
-
     Cell(int r, int c){
         this.r = r;
         this.c = c;
