18e93b801639c692f498730851fa775907c3b6d7
Rithish S <rithishcodespace@gmail.com>
2026-09-01 11:11:33 +0530

    Time: 0 ms (100%), Space: 42.4 MB (7.01%) - LeetHub

--- 0991-broken-calculator/0991-broken-calculator.java
+++ 0991-broken-calculator/0991-broken-calculator.java
@@ -1,17 +1,18 @@
-// go from target to start value
+// make target equals to startValue
 
 class Solution {
     public int brokenCalc(int startValue, int target) {
         int operations = 0;
+        
         while(target > startValue){
-            if(target % 2 == 0){
+            if(target%2 == 0){
                 target /= 2;
             }
-            else{
-                target++;
-            }
+            else target++;
+
             operations++;
         }
-        return operations + startValue-target; // startValue is larger
+
+        return operations + startValue-target;
     }
 }
