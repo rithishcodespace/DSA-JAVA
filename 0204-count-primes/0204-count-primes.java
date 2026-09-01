4adcebf6817e0c6ea1a03b3e8b5fa6fba81ef490
Rithish S <rithishcodespace@gmail.com>
2026-09-01 18:35:04 +0530

    Time: 766 ms (10.21%), Space: 79.7 MB (20.07%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,23 +1,31 @@
+// multiples of primes are always composite
+// after root(n), we will get same relations (already seen)
+
 class Solution {
     public int countPrimes(int n) {
         if(n <= 1)return 0;
 
-        boolean[] primes = new boolean[n]; 
+        boolean[] primes = new boolean[n];
+        
         Arrays.fill(primes, true);
         primes[0] = false;
         primes[1] = false;
 
+        // mark the composite numbers
         for(int i=2;i<Math.sqrt(n);i++){
-            if(primes[i] == true){
+            if(primes[i]){
                 for(int j=i*i;j<n;j+=i){
                     primes[j] = false;
                 }
             }
         }
 
+        // count the prime numbers
         int count=0;
         for(int i=0;i<n;i++){
-            count += primes[i] ? 1 : 0;
+            if(primes[i]){
+                count++;
+            }
         }
 
         return count;
