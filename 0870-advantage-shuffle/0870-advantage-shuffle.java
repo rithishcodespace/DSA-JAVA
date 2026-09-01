5976d896255591f488517f0eb9ab3b126e433534
Rithish S <rithishcodespace@gmail.com>
2026-09-01 18:59:52 +0530

    Time: 60 ms (74.85%), Space: 72.2 MB (80.59%) - LeetHub

--- 0870-advantage-shuffle/0870-advantage-shuffle.java
+++ 0870-advantage-shuffle/0870-advantage-shuffle.java
@@ -1,25 +1,24 @@
 class Solution {
     public int[] advantageCount(int[] nums1, int[] nums2) {
-        int[] res = new int[nums1.length];
+        int[] ans = new int[nums1.length];
 
+        // sort nums1
         Arrays.sort(nums1);
 
-        Integer[] idx = new Integer[nums1.length];
-        for(int i=0;i<nums1.length;i++)idx[i]=i;
-
+        // sort nums2, but without modifying it
+        Integer[] idx = new Integer[nums2.length]; // primitive can't be sorted with Arrays.sort
+        for(int i=0;i<nums2.length;i++)idx[i]=i;
         Arrays.sort(idx, (a,b) -> nums2[b]-nums2[a]);
 
+        // process each's max val and decide whom to take
         int l=0, r=nums1.length-1;
-
         for(int i : idx){
             if(nums1[r] > nums2[i]){
-                res[i] = nums1[r--];
+                ans[i] = nums1[r--];
             }
-            else{
-                res[i] = nums1[l++];
-            }
+            else ans[i] = nums1[l++];
         }
 
-        return res;
+        return ans;
     }
 }
