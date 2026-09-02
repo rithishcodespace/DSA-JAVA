7c11e11271cbd81a75de23134fe3891c3c13523b
Rithish S <rithishcodespace@gmail.com>
2026-09-02 08:58:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,38 +1,22 @@
-// binary search
+// tabulation
+// dp[i] - lis ending at i
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        int[] dp = new int[nums.length];
+
+        Arrays.fill(dp, 1); // each index is a lis of length 1
 
         for(int i=0;i<nums.length;i++){
-            int num = nums[i];
-            if(list.isEmpty() || list.get(list.size()-1) < num){
-                list.add(num);
-            }
-            else{ // instead of creating new list from scratch, place num in lower bound index
-                int idx = lowerBound(0, list.size()-1, num, list);
-                list.set(idx, num);
-            }        
-        }
-
-        return list.size();
-    }
-    // lb => index >= num
-    public int lowerBound(int l, int r, int target, List<Integer> list){
-        int idx = 0;
-
-        while(l<=r){
-            int mid = l + (r-l)/2;
-
-            if(list.get(mid) >= target){
-                idx=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    if(dp[j]+1 > dp[i]){
+                        dp[i] = dp[j]+1;
+                    }
+                }
             }
         }
 
-        return idx;
+        return dp[nums.length-1];
     }
 }
