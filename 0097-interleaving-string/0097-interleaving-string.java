d11d9572ed43e4394dd4928b5fec7eb513e392a8
Rithish S <rithishcodespace@gmail.com>
2026-09-22 18:03:46 +0530

    Time: 0 ms (100%), Space: 43.5 MB (16.06%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,33 +1,30 @@
-// k -> idx3 (position on s3, to be created)
-// call only if s1 or s2's index can match with s3's character
+// build the s3, by checking either idx1 or idx2 matches the currently searching idx3 (k)
 
 class Solution {
-    Boolean[][] dp;
-
     public boolean isInterleave(String s1, String s2, String s3) {
-        dp = new Boolean[s1.length()+1][s2.length()+1];
-        return solve(0, 0, s1, s2, s3);
+        if(s1.length()+s2.length() != s3.length())return false;
+        Boolean[][] dp = new Boolean[s1.length()+1][s2.length()+1];
+        return solve(0, 0, s1, s2, s3, dp);
     }
-
-    public boolean solve(int idx1, int idx2, String s1, String s2, String s3){
-        int k = idx1 + idx2;
-
-        if(k >= s3.length()){
-            return (idx1 >= s1.length() && idx2 >= s2.length()) ? true : false;
-        }
+    public boolean solve(int idx1, int idx2, String s1, String s2, String s3, Boolean[][] dp){
+        int k = idx1+idx2; // idx3
 
         if(dp[idx1][idx2] != null)return dp[idx1][idx2];
 
-        boolean pickFrom1 = false;
+        if(idx1 == s1.length() && idx2 == s2.length()){
+            return true;
+        }
+
+        boolean res = false;
+
         if(idx1 < s1.length() && s1.charAt(idx1) == s3.charAt(k)){
-            pickFrom1 = solve(idx1+1, idx2, s1, s2, s3);
+            res = res || solve(idx1+1, idx2, s1, s2, s3, dp);
         }
-
-        boolean pickFrom2 = false;
         if(idx2 < s2.length() && s2.charAt(idx2) == s3.charAt(k)){
-            pickFrom2 = solve(idx1, idx2+1, s1, s2, s3);
+            res = res || solve(idx1, idx2+1, s1, s2, s3, dp);
         }
 
-        return dp[idx1][idx2] = pickFrom1 || pickFrom2;
+
+        return dp[idx1][idx2] = res;
     }
 }
